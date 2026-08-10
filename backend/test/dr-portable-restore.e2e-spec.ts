c3d7dd4535b1c07d78a55996ef4e99cd65aa866e
PLANT03 <PLANT03@sja.com>
2026-08-10 15:50:42 +0700

    fix(test): import path in dr-portable-restore.e2e-spec.ts

--- backend/test/dr-portable-restore.e2e-spec.ts
+++ backend/test/dr-portable-restore.e2e-spec.ts
@@ -1,6 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as fs from 'fs';
+import * as path from 'path';
 import * as argon2 from 'argon2';
 import { AppModule } from './../src/app.module';
 import { PrismaService } from './../src/prisma/prisma.service';
